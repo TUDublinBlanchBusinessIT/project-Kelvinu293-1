3dc87a7327e0c3d997f38ee94c6a73cdd660b57a
Kelvin Uzo <b00141682@mytudublin.ie>
2024-11-23 02:54:44 +0000

    start of login and logo

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Change!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image
+            source={require('./assets/bondgologo.png')} 
+            style={styles.headerImg}
+          />
+          <Text style={styles.title}>Sign in To Myapp</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#eBecf4',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
+  },
+  header: {
+    marginVertical: 36,
+  },
+  headerImg: {
+    width: 80,
+    height: 80,
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: 27,
+    fontWeight: '700',
+    color: '#dedede',
+    textAlign: 'center', 
   },
 });
