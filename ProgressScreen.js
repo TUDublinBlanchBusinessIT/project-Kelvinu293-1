753d9693a686eb3c74e44775c3464820dfb5fada
Kelvin Uzo <b00141682@mytudublin.ie>
2024-12-05 00:05:37 +0000

    start of my progress bar

--- ProgressScreen.js
+++ ProgressScreen.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { ProgressBar } from 'react-native-paper'; // Ensure this package is installed
 
 export default function ProgressScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Progress Screen</Text>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
-        <Text style={styles.buttonText}>Go Back to Home</Text>
-      </TouchableOpacity>
+      <ProgressBar progress={0.5} color="yellow" style={styles.progressBar} /> 
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: 'pink',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  progressBar: {
+    width: '80%',
+    height: 40,
+    borderRadius: 5,
+    marginVertical: 20,
+  },
+
+});
+
+
+
 
 
 
