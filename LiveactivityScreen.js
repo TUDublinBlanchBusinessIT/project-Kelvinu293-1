44f6a14189cce60d7dce9e1a40af2a7a3a06dff2
Kelvin Uzo <b00141682@mytudublin.ie>
2024-12-06 22:18:44 +0000

    liveactivity screen done

--- LiveactivityScreen.js
+++ LiveactivityScreen.js
@@ -3,6 +3,10 @@ import { Text, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-nat
 
 const Ima = require("./camping.png");
 const pic = require("./concert.png");
+const bow = require("./bowling.png");
+const cas = require("./castle.png");
+const zoo = require("./zoo.png");
+
 
 export default function LiveactivityScreen({ navigation }) {
   return (
@@ -10,11 +14,16 @@ export default function LiveactivityScreen({ navigation }) {
       <Text style={styles.title}>Live Activity Screen</Text>
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
       </TouchableOpacity>
-      <Image source={Ima} style={{ width: 300, height: 300 }} />
+      <Image source={Ima} style={{ width: 350, height: 300 }} />
       <Text>Come to camp in the wild fire</Text>
-      <Image source={pic} style={{ width: 300, height: 300 }} />
+      <Image source={pic} style={{ width: 350, height: 300 }} />
       <Text>The boys rocker is playing this week in george pub, come through with the family</Text>
-
+      <Image source={bow} style={{ width: 350, height: 300 }} />
+      <Text>Come bowling in Blanchardstwon Leisureplex</Text>
+      <Image source={cas} style={{ width: 350, height: 300 }} />
+      <Text>Visit Castle Night in 30th April</Text>
+      <Image source={zoo} style={{ width: 350, height: 200 }} />
+      <Text>30% off as Family in Dublin Zoo until August 15th</Text>
     </ScrollView>
   );
 }
@@ -24,6 +33,7 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     alignItems: 'center',
     padding: 20,
+    backgroundColor: 'yellow'
     
   },
   title: {
